e939cc17b2cfaa7871e877fec37506e1d4b529b9
Papa Ree <79291092+papa-ree@users.noreply.github.com>
2026-04-15 10:36:27 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <x-emperan::seo-meta 
+    <x-seo::seo-meta 
         :model="$seoModel ?? null" 
         :defaults="[
             'title' => $title ?? 'Dinas Tenaga Kerja Kabupaten Ponorogo', 
